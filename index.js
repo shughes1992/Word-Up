e92e4117ebb257664496c08e5b0ec4105b963dc6
Samantha Hughes <hughes2@law.upenn.edu>
2018-06-03 18:07:15 -0400

    Beginning folder structure

---
+++ index.js
@@ -0,0 +1,12 @@
+* **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+* Randomly selects a word and uses the `Word` constructor to store it
+* Prompts the user for each guess and keeps track of the user's remaining guesses
+
+
+var Game = require("./lib/Game");
+
+// Initialize a new Game object
+var game = new Game();
+
+// Start playing
+game.play();
